955511959744908c38ed68f62cccb8299621c155
Pritam Nagar <130887196+Nagar-18@users.noreply.github.com>
2024-01-17 21:12:11 +0530

    Update ViewTask.jsx

--- cleint/src/pages/ViewTask.jsx
+++ cleint/src/pages/ViewTask.jsx
@@ -40,7 +40,7 @@ const ViewTask = () => {
       
        <div style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
        
-     <form onSubmit={handleOnSubmit} style={{height:'40vh', width:'40vw',alignContent:'center',padding:'4vh 4vh',border:' 2px solid white'}}>
+     <form onSubmit={handleOnSubmit} style={{height:'40vh', width:'50vw',alignContent:'center',padding:'4vh 4vh',border:' 2px solid white'}}>
        <h2>View Your Note Here</h2>
        {flag?<> <div style={{margin:'8px'}}> <input ref={idRef} type="text" placeholder='Enter id of the task' />
        </div> <button type='submit'>Submit </button></>:<>
